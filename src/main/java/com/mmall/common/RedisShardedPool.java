8bd1b0f8b34296888d2180862caa85f927c18dd0
bright <783640551@qq.com>
2019-04-05 18:17:26 +0800

    add springsession

--- src/main/java/com/mmall/common/RedisShardedPool.java
+++ src/main/java/com/mmall/common/RedisShardedPool.java
@@ -1,7 +1,10 @@
 package com.mmall.common;
 
 import com.mmall.util.PropertiesUtil;
-import redis.clients.jedis.*;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
 import redis.clients.util.Hashing;
 import redis.clients.util.Sharded;
 
